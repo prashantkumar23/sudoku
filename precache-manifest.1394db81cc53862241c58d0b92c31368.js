39f12f9d716395b4d4a4d90e4b34d33775d2fed1
Prashant <blackhole.gamma@gmail.com>
2021-03-04 14:25:04 +0530

    Updates

---
+++ precache-manifest.1394db81cc53862241c58d0b92c31368.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c28abbed57b10f0cfcabe0939efee52",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "da104999b0997392b9dd",
+    "url": "/sudoku/static/js/2.684611ee.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/sudoku/static/js/2.684611ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18cfa969d703350f37a",
+    "url": "/sudoku/static/js/main.91cf6c97.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  }
+]);
